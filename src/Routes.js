8cbd5c5711e5579f106212a4db6a4bb1b1ebcdf8
Piotr Leniec <leniec.piotr@gmail.com>
2017-03-26 13:28:19 +0200

    9. Static "LogInForm" component

--- src/Routes.js
+++ src/Routes.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
 import Lunches from './components/Lunches'
+import LogInForm from './components/LogInForm'
 
 export default () => (
   <BrowserRouter>
-    <Route exact path="/" component={Lunches} />
+    <div>
+      <Route exact path="/" component={Lunches} />
+      <Route exact path="/login" component={LogInForm} />
+    </div>
   </BrowserRouter>
 )
---
+++ src/components/LogInForm.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default () => (
+  <div className="row">
+    <div className="col-xs-offset-4 col-xs-4">
+      <div className="panel panel-primary">
+        <div className="panel-heading">
+          Enter your name
+        </div>
+        <div className="panel-body">
+          <div className="form-group">
+            <input className="form-control" />
+          </div>
+          <button className="btn btn-block btn-primary">Log in</button>
+        </div>
+      </div>
+    </div>
+  </div>
+)
